9095b6a41aba97497144bd500479f8d939936144
igorvreis1 <54328542+igorvreis1@users.noreply.github.com>
2021-10-14 10:19:12 -0300

    Add files via upload

---
+++ autoPianoTiles.py
@@ -0,0 +1,53 @@
+import time
+
+from PIL import ImageGrab
+import pyautogui
+
+
+
+#tecla = (1, 1, 1)
+#teclaStart = (54, 159, 198)
+#teclaArrastada = (0, 2, 3)
+#teclaAmarela = (255, 183, 9)
+
+def printScrean():
+    imagem = ImageGrab.grab()
+    #color = screen.getpixel((680, 527))
+
+    #print(color, time.process_time())
+    return (imagem)
+
+def procuraTecla(imagem):
+    #rangeD = (650,508)
+    #rangeF = (735,508)
+    #rangeJ = (815,508)
+    #rengek = (895,508)
+    cont = 0
+
+    ranges = ((650,508),(735,508),(815,508),(895,508))
+    for i in ranges:
+        color = imagem.getpixel(i)
+        if color == (1, 1, 1) or color == (54, 159, 198) or color == (0, 2, 3) or color == (255, 183, 9):
+            return cont
+        cont+=1
+    return -1
+
+def aperta(tecla):
+    if tecla == 0:
+        pyautogui.press("d")
+    elif tecla == 1:
+        pyautogui.press("f")
+    elif tecla == 2:
+        pyautogui.press("j")
+    else:
+        pyautogui.press("k")
+
+
+print("Começa em 3 segundos, vá para a tela do jogo.")
+time.sleep(3)
+
+while True:
+    imagem = printScrean()
+    tecla = procuraTecla(imagem)
+    if tecla != -1:
+        aperta(tecla)
